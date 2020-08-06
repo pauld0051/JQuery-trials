768a5f9d0caf4f7ae0333c9b7cb0830ebb930418
pauld0051 <pauld0051@gmail.com>
2020-08-06 15:31:31 +0000

    corrected link to js file and updated my_script

--- js/my_script.js
+++ js/my_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   //add any scripts here, they will only load after the DOM fully loads
   $("h2").addClass("underline"); //underlines all the h2 elements 
-  $("ul").addClass("border"); //adds a solid border to all ul 
+  $("h2").addClass("border"); //adds a solid border to all ul 
 });
