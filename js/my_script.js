da1c88554bcbafcf29fdb0ead997fd4346685c48
pauld0051 <pauld0051@gmail.com>
2020-08-06 15:07:34 +0000

    added jquery and js and table for the html

---
+++ js/my_script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  //add any scripts here, they will only load after the DOM fully loads
+  $("h2").addClass("underline"); //underlines all the h2 elements 
+  $("ul").addClass("border"); //adds a solid border to all ul 
+});
